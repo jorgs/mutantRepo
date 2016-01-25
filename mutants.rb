42b3a76f45ace540908fa76d5d7b27946d64c93b
Danny Jorgenson <djorgenson@salesforce.com>
2016-01-25 10:57:37 -0500

    encapsulate app in object

--- mutants.rb
+++ mutants.rb
@@ -1,19 +1,23 @@
 #!/usr/bin/env ruby
-puts 'awww, yeah. mutants'
-mutant_array =Array.new
-single_mutants_array = Array.new
-puts 'how many mutants on your legally distinct but similar to xmen team'
-number = gets.chomp.to_i
-until number == 0 do
-puts 'what is your real name'
-real_name = gets.chomp
-puts 'what is your mutant name'
-mutant_name = gets.chomp
-puts 'what is your mutant power'
-mutant_power = gets.chomp
-puts "name #{real_name} mutant name #{mutant_name} mutant power is #{mutant_power}"
-single_mutants_array.push(real_name, mutant_name, mutant_power)
-mutant_array.push(mutant_array)
-puts mutant_array
-  number = number - 1
+class MutantsApplication
+  def self.get_more_mutants
+    mutant_array =Array.new
+    single_mutants_array = Array.new
+    puts 'how many mutants on your legally distinct but similar to xmen team'
+    number = gets.chomp.to_i
+    until number == 0 do
+      puts 'what is your real name'
+      real_name = gets.chomp
+      puts 'what is your mutant name'
+      mutant_name = gets.chomp
+      puts 'what is your mutant power'
+      mutant_power = gets.chomp
+      puts "name #{real_name} mutant name #{mutant_name} mutant power is #{mutant_power}"
+      single_mutants_array.push(real_name, mutant_name, mutant_power)
+      mutant_array.push(mutant_array)
+      puts mutant_array
+      number = number - 1
+  end
+end
+  MutantsApplication.get_more_mutants
 end
